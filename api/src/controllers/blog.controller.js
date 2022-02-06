562bddb91114e16e4db14a150c227a602b219e5f
dtamta007 <dtamta007@gmail.com>
2022-02-06 18:24:09 +0530

    First commit

---
+++ api/src/controllers/blog.controller.js
@@ -0,0 +1,12 @@
+const createBlog = (req,res) =>{
+    //Creating blog logic here
+};
+
+const getAllBlogs = (req,res) =>{
+    //Get all Blogs logic here
+}
+
+const AdsController = {
+    createBlog,
+    getAllBlogs
+}
---
+++ api/src/routes/blog.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const BlogController = require('../controllers/blog.controller');
+
+// Create routes for blogs here
+module.exports = router;
